250770732660b05fe53ad62b10a3be2157c5bdf6
Tera Earlywine <dev@teraearlywine.com>
2024-09-11 09:51:31 -0700

    *

---
+++ MyResume/App.js
@@ -0,0 +1,55 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+
+import ImageViewer from './components/ImageViewer'; 
+import Button from './components/Button';
+
+import * as ImagePicker from 'expo-image-picker';
+
+const PlaceholderImage = require('./assets/images/background-image.png');
+
+export default function App() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+      <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+        <Button label="Use this photo" />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  // Styles that are unchanged from previous step are hidden for brevity. 
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },  
+  container: {
+    flex: 1,
+    backgroundColor: '#1e1e1e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+});
---
+++ docs/react-native-cli.md
@@ -0,0 +1,15 @@
+# Commands 
+
+
+```zsh
+# CD into react app sub-directory. 
+# cd SubDirectoryAppName
+cd MyResume 
+
+# Using EXPO, starts web (w) and ios (i) interfaces for testing
+npx expo start
+
+# Stopping the server: Ctrl + C
+
+
+```
