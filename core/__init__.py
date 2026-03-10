3d3b205b09955c7fbb608464c9944eaa883bce44
Tera Earlywine <dev@teraearlywine.com>
2026-03-10 09:30:11 -0700

    *

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import secrets
 
 from dotenv import load_dotenv
 from flask import Flask, render_template
@@ -40,16 +41,26 @@ def create_app(env=''):
     env = env or os.environ.get('FLASK_ENV', 'production')
     app.config.from_object(app_config.get(env, app_config['default']))
 
-    secret_key = os.environ.get('SECRET_KEY')
-    if not secret_key:
-        raise RuntimeError("SECRET_KEY environment variable is not set")
-    app.secret_key = secret_key
-
     logging.basicConfig(
         level=app.config['LOG_LEVEL'],
         datefmt="%Y-%m-%d",
         format="%(levelname)s - %(message)s"
     )
+
+    secret_key = os.environ.get('SECRET_KEY')
+    if not secret_key:
+        if env == 'production':
+            secret_key = secrets.token_hex(32)
+            app.logger.warning(
+                "SECRET_KEY environment variable is not set; using an ephemeral key for this process"
+            )
+        else:
+            secret_key = 'development-secret-key'
+            app.logger.warning(
+                "SECRET_KEY environment variable is not set; using the default development key"
+            )
+    app.config['SECRET_KEY'] = secret_key
+    app.secret_key = secret_key
 
     register_blueprints(app)
     register_error_handlers(app)
